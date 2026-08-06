dcd9514bb730ceea8024907932e5ebcbf9e14d74
riajuddin1144-code <riajuddin1144@gmail.com>
2026-08-06 15:48:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function topup() {
-  let uid = document.getElementById("uid").value;
-  let diamond = document.getElementById("diamonds").value;
-  let msg = document.getElementById("msg");
-
-  if (uid === "" || diamond === "") {
-    msg.innerHTML = "❌ Please enter UID and select diamonds!";
-    msg.style.color = "red";
-  } else {
-    msg.innerHTML = "✅ Demo Success! (No real top-up)";
-    msg.style.color = "lime";
-  }
+function pay() {
+  alert("Demo Payment Only ⚠️");
 }
